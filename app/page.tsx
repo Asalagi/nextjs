6fd65ff4b486a95125588e909fe395e578e2697a
Asalagi <mypaintedfrog@yahoo.com>
2023-11-02 17:17:51 -0500

    add simple layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,17 @@ export default function Home() {
   return (
     <main>
       <div>
+        <div className="main-container">
+        <div className="main-header">
+          link or something like that will go here.
+        </div>
+        <div className="inner-content">
         <h1>Test - Add something here</h1>
+        </div>
+        <div className="main-footer">
+          here is a simple footer
+        </div>
+        </div>
       </div>
     </main>
   )
