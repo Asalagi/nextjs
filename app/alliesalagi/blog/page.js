6523ef4afe5ebc0d89bc93e9b6ae5fed8c75fbd7
Asalagi <mypaintedfrog@yahoo.com>
2023-11-08 16:12:23 -0600

    add rest of the pages

---
+++ app/alliesalagi/blog/page.js
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import '../styles.css';
+
+function Blog () {
+    return (
+    <div>
+      <div className="main-content">
+        <h1 className="title">Allie Salagi</h1> 
+        <div className="outer-box">
+            <div className="content-box">
+            <div className="nav-bar">
+                <Link href="/alliesalagi" className="link"><button className="nav-btn">Home</button></Link>
+                <Link href="/alliesalagi/about" className="link"><button className="nav-btn">About</button></Link>
+                <Link href="/alliesalagi/resume" className="link"><button className="nav-btn">Resume</button></Link>
+                <Link href="/alliesalagi/blog" className="link"><button className="nav-btn">Blog</button></Link>
+                <Link href="/alliesalagi/contact" className="link"><button className="nav-btn">Contact</button></Link>
+            </div>
+          <p>{"Put blog posts here"}</p></div>
+        </div>
+      </div>
+    </div>
+    )
+}
+
+export default Blog;
