7759cf05c47eeb6b480467bbd9823ac451fbd7c0
Asalagi <mypaintedfrog@yahoo.com>
2023-11-21 15:19:17 -0600

    start calendar

---
+++ app/calendar/page.js
@@ -0,0 +1,29 @@
+import './calendar.css';
+
+function Calendar(){
+    const date = new Date();
+    const month = date.toLocaleString("en-US", {
+        month: "long"
+    });
+
+    return (
+    <div>
+        <div className="calendar-wrapper">
+            <div className="title-container">
+                <h1 className="month-title">{month}</h1>
+            </div>
+            <div className="calendar-table">
+                <div className="week-box">Sunday</div>
+                <div className="week-box">Monday</div>
+                <div className="week-box">Tuesday</div>
+                <div className="week-box">Wednesday</div>
+                <div className="week-box">Thursday</div>
+                <div className="week-box">Friday</div>
+                <div className="week-box">Saturday</div>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default Calendar;
