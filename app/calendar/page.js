2c0d670f76e8ee58ced983d4dc5ea251bbbfc3e8
Asalagi <mypaintedfrog@yahoo.com>
2023-11-27 16:04:17 -0600

    remove files

--- app/calendar/page.js
+++ app/calendar/page.js
@@ -3,9 +3,7 @@ import { useState } from 'react';
 import './calendar.css';
 
 function Calendar(){
-"/"
-  
-let days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+    let days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
     let months = ["January", "February", "March", "April", "May", "June", "July",
     "August", "September", "October", "November", "December"];
     return (
@@ -13,7 +11,7 @@ let days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 2
         <div className="calendar-wrapper">
             <div className="title-container">
                 <h1 className="month-title">
-                    <button>Pev</button>{months[10]}<button>Next</button></h1>
+                    <button>Pev</button>{months[0]}<button>Next</button></h1>
             </div>
         <div className="calendar-table">
             {days.map(days => (
