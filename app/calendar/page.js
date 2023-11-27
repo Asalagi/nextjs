be246085471c56e6249f24ddeb2f68d79c2da2b0
Asalagi <mypaintedfrog@yahoo.com>
2023-11-27 15:55:03 -0600

    add key

--- app/calendar/page.js
+++ app/calendar/page.js
@@ -3,8 +3,9 @@ import { useState } from 'react';
 import './calendar.css';
 
 function Calendar(){
-    const [month, setMonth] = useState(0);
-    let days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+"/"
+  
+let days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
     let months = ["January", "February", "March", "April", "May", "June", "July",
     "August", "September", "October", "November", "December"];
     return (
@@ -16,7 +17,7 @@ function Calendar(){
             </div>
         <div className="calendar-table">
             {days.map(days => (
-                <div className='week-box'></div>
+                <div className='week-box' key={days}></div>
             ))}
         </div>
 
