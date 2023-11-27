d209d67cee997de73b0179cad3d06139d6612fa1
Asalagi <mypaintedfrog@yahoo.com>
2023-11-27 15:41:39 -0600

    calendar days

--- app/calendar/page.js
+++ app/calendar/page.js
@@ -3,43 +3,23 @@ import { useState } from 'react';
 import './calendar.css';
 
 function Calendar(){
-    let today = new Date();
+    const [month, setMonth] = useState(0);
+    let days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
     let months = ["January", "February", "March", "April", "May", "June", "July",
     "August", "September", "October", "November", "December"];
-    let currentMonth = months[today.getMonth()];
-    let year = today.getFullYear();
-
-    let prev = months[today.getMonth() - 1];
-    let next = months[today.getMonth() + 1];
-
-    const [count, setCount] = useState(0);
-
-    function handlePrevChange(){
-        setCount((count) => count - 1);
-        console.log(count)
-    }
-
-    function handleNextChange(){
-        setCount((count) => count + 1);
-        console.log(count)
-    }
-
     return (
     <div>
         <div className="calendar-wrapper">
             <div className="title-container">
                 <h1 className="month-title">
-                    <button onClick={handlePrevChange}>Pev</button>{months[0]} {year}<button onClick={handleNextChange}>Next</button></h1>
+                    <button>Pev</button>{months[10]}<button>Next</button></h1>
             </div>
-            <div className="calendar-table">
-                <div className="week-box">Sunday</div>
-                <div className="week-box">Monday</div>
-                <div className="week-box">Tuesday</div>
-                <div className="week-box">Wednesday</div>
-                <div className="week-box">Thursday</div>
-                <div className="week-box">Friday</div>
-                <div className="week-box">Saturday</div>
-            </div>
+        <div className="calendar-table">
+            {days.map(days => (
+                <div className='week-box'></div>
+            ))}
+        </div>
+
         </div>
     </div>
     )
