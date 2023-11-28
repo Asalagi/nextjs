ff5c7e978503f1b626fa4f1cfc08b1f6281afc30
Asalagi <mypaintedfrog@yahoo.com>
2023-11-28 11:43:08 -0600

    add function to buttons

--- app/calendar/page.js
+++ app/calendar/page.js
@@ -13,15 +13,26 @@ function Calendar(){
         return days;
     };
 
-
     let months = ["January", "February", "March", "April", "May", "June", "July",
     "August", "September", "October", "November", "December"];
+
+    const handlePrevMonth = () => {
+        setMonth(month - 1)
+    }
+
+    const handleNextMonth = () => {
+        setMonth(month + 1)
+    }
+
     return (
     <div>
         <div className="calendar-wrapper">
             <div className="title-container">
                 <h1 className="month-title">
-                    <button className="calendar-btn" onClick={()=> alert('this is a previous button')}>Pev</button> {months[month]} <button className="calendar-btn" onClick={() => alert('this is a next button')}>Next</button></h1>
+                    <button className="calendar-btn" onClick={handlePrevMonth}>Pev</button> 
+                        {months[month]} 
+                    <button className="calendar-btn" onClick={handleNextMonth}>Next</button>
+                </h1>
             </div>
         <div className="calendar-table">
             {dayLoop().map(days => (
