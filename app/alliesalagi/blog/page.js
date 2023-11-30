c8ea391393920d666dfbb7c6d7c37aedbb3d1670
Asalagi <mypaintedfrog@yahoo.com>
2023-11-30 11:48:59 -0600

    add horse project

--- app/alliesalagi/blog/page.js
+++ app/alliesalagi/blog/page.js
@@ -1,5 +1,8 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import '../styles.css';
+import horseshop1 from '../horseshop1.jpg';
+import horseshop2 from '../horseshop2.jpg';
 
 function Blog () {
     return (
@@ -24,6 +27,12 @@ function Blog () {
               <h2>{"Calendar Project"}</h2>
               <p>{"Check it out"} <Link href="../calendar" target="_blank" className="link">Here</Link><br/>
               {"This calendar project is a blank slate calendar. Featuring a title with the month's name and a previous month and next month button."}</p>
+              <hr/>
+              <h2>{"Horse Shop Project"}</h2>
+              <p>{"I had a lot of fun with this project combiding my love for horses and love of learning programming. This project features a list of horses, stored in an array and mapped over, holding each horses information in it's own card styled with flexbox. When add cart is clicked below a horses card it is then added to the shopping cart (upper right corner)."}</p>
+              <Image src={horseshop1} className="image" fill={true} alt="horseshop1"/>
+              <p>{"When the cart link from the image above is clicked it will take you to a screen that has the horses you added to the cart as well as you total stored in the cart summary on the right hand side."}</p>
+              <Image src={horseshop2} className="image" fill={true} alt="horseshop2"/>
           </div>
         </div>
       </div>
