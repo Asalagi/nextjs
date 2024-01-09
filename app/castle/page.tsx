923d86ce6d3f223b066045223677f9507423201f
Asalagi <mypaintedfrog@yahoo.com>
2024-01-09 16:09:56 -0600

    change layout didnt like first

--- app/castle/page.tsx
+++ app/castle/page.tsx
@@ -3,17 +3,21 @@ import './styling.css';
 function Castle() {
 
     return (
-        <div className="container">
-            <div className="top">
-                This is the top.
-            </div>
-            <div className="lower-container">
-                <div className="side-container">
-                    this is a side container
+        <div className="outer-container">
+            <div className="title">Game Title Here</div>
+            <div className="container-main">
+                <div className="card">
+                    Name, Class, Weapon, Life Bar, Money, Stats
                 </div>
-                <div className="main-content">
-                    This is the main box
-                </div>
+                <div className="display">Display messages here</div>
+                <div className="inner-display">special messages here</div>
+                <div className="btn-container">
+                    <button>button</button>
+                    <button>button</button>
+                    <button>button</button>
+                    <button>button</button>
+                    <button>button</button>
+                    <button>button</button></div>
             </div>
         </div>
     );
