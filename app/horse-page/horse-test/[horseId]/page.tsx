87be04573243ea0c30eb55fb6a4a290a6340bbfb
Asalagi <mypaintedfrog@yahoo.com>
2024-02-19 14:23:14 -0600

    try different call for json

--- app/horse-page/horse-test/[horseId]/page.tsx
+++ app/horse-page/horse-test/[horseId]/page.tsx
@@ -1,13 +1,27 @@
-// OLD CODE TO BE USED WITH DATA ACCESS
-import { getHorseById, Horse } from '@/app/data-access/horses';
+// NEW CODE LETS TRY
+import { promises as fs } from 'fs';
+
+export interface Horse {
+    id: number;
+    name: string;
+    breed: string;
+    age: number;
+    color: string;
+    sex: string;
+    height: string;
+    notes: string;
+}
 
 export default async function getHorse({ params }: {params: {horseId: string}}) {
-  
-  const horseId = parseInt(params.horseId, 10);
-  console.log(horseId, params);
+    const file = await fs.readFile(process.cwd() + '/public/horses.json', 'utf8');
+    const horsesData = JSON.parse(file);
+    const horseId = parseInt(params.horseId, 10);
+
+    const selectedHorse = horsesData.find((horse: Horse) => horse.id === horseId);
+      console.log(horseId, params);
+      console.log(selectedHorse);
 
   try {
-    const selectedHorse: Horse = await getHorseById(horseId);
     if (!selectedHorse) {
       return <div>Horse not found</div>;
     }
@@ -21,30 +35,24 @@ export default async function getHorse({ params }: {params: {horseId: string}}) 
     );
   } catch (error) {
     console.log('Error reading or parsing JSON file:', error);
+
     return <div>Error loading data</div>;
   }
 }
 
-// import { promises as fs } from 'fs';
 
-// export interface Horse {
-    // id: number;
-    // name: string;
-    // breed: string;
-    // age: number;
-    // color: string;
-    // sex: string;
-    // height: string;
-    // notes: string;
-// }
+// OLD CODE TO BE USED WITH DATA ACCESS
+// import { getHorseById, Horse } from '@/app/data-access/horses';
 
 // export default async function getHorse({ params }: {params: {horseId: string}}) {
-//   const file = await fs.readFile(process.cwd() + '/public/horses.json', 'utf8');
-//   const horsesData: Horse[] = JSON.parse(file);
+  
 //   const horseId = parseInt(params.horseId, 10);
+//   console.log(horseId, params);
 
 //   try {
-//     const selectedHorse = horsesData.find((horse: Horse) => horse.id === horseId);
+//     const selectedHorse: Horse = await getHorseById(horseId);
+//     console.log(selectedHorse);
+
 //     if (!selectedHorse) {
 //       return <div>Horse not found</div>;
 //     }
@@ -57,7 +65,8 @@ export default async function getHorse({ params }: {params: {horseId: string}}) 
 //       </div>
 //     );
 //   } catch (error) {
-//     console.error('Error reading or parsing JSON file:', error);
+//     console.log('Error reading or parsing JSON file:', error);
+
 //     return <div>Error loading data</div>;
 //   }
 // }
