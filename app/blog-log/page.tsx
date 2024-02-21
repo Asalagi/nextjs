e883b0162069db803af0c897ad407102219d739b
Asalagi <mypaintedfrog@yahoo.com>
2024-02-21 17:05:00 -0600

    add collapse and map over

--- app/blog-log/page.tsx
+++ app/blog-log/page.tsx
@@ -1,24 +1,48 @@
+'use client';
+import { useState } from 'react';
 import './blog-styling.css';
 import Image from 'next/image';
 import topImage from './top.jpg';
 
 export default function BlogLog () {
-    const blogData = {
-        title: "A New Day",
-        date: "21st February 2024",
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    const [fullPost, setFullPost] = useState(false);
 
-        };
+    const showContent = () => {
+        setFullPost(true)
+    }
+
+    const blogData = [
+        {
+            id: 5,
+            title: "A New Day",
+            date: "21st February 2024",
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        },
+    ];
+
+    const contentWords = (content: string, maxLength: number) => {
+        if (content.length <= maxLength) return content;
+
+        const lastSpaceIndex = content.lastIndexOf(' ', maxLength);
+        return content.slice(0, lastSpaceIndex) + '...';
+    };
+
     return (
         <div>
             <div className="main-container">
                 <Image src={topImage} className="flexImage" alt="top image"/>
                 <h1 className="title">Welcome to the Blog Log</h1>
-                <div className="post-container">
-                    <h2 className="post-title">{blogData.title}</h2>
-                    <p className="post-date">{blogData.date}</p><br/>
-                    <p className="post-content">{blogData.content}</p>
-                </div>
+                {blogData.map((post, id) => (
+                    <div className="post-container" key={id}>
+                        <h2 className="post-title">{post.title}</h2>
+                        <p className="post-date">{post.date}</p><br/>
+                        <p className="post-content">{fullPost ? post.content : contentWords(post.content, 130)}</p>
+                        {!fullPost && (
+                            <p onClick={showContent} className="see-more">See more</p>
+                        )}
+
+                    </div>
+                ))}
             </div>
         </div>
     );
