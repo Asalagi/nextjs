8b9f013793c5bf599337794b86e2702747691e10
Asalagi <mypaintedfrog@yahoo.com>
2024-02-23 09:38:44 -0600

    fix flex box hieght

--- app/blog-log/page.tsx
+++ app/blog-log/page.tsx
@@ -24,17 +24,19 @@ export default function BlogLog () {
             <div className="main-container">
                 <Image src={topImage} className="flexImage" alt="top image"/>
                 <h1 className="title">Welcome to the Blog Log</h1>
-                {blogPost.map((post) => (
-                    <div className="post-container" key={post.id}>
-                        <h2 className="post-title">{post.title}</h2>
-                        <p className="post-date">{post.date}</p><br/>
-                        <p className="post-content">{fullPost ? post.content : contentWords(post.content, 130)}</p>
-                        {!fullPost && (
-                            <p onClick={showContent} className="see-more">See more</p>
-                        )}
+                <div className="content-container">
+                    {blogPost.map((post) => (
+                        <div className="post-container" key={post.id}>
+                            <h2 className="post-title">{post.title}</h2>
+                            <p className="post-date">{post.date}</p><br/>
+                            <p className="post-content">{fullPost ? post.content : contentWords(post.content, 130)}</p>
+                            {!fullPost && (
+                                <p onClick={showContent} className="see-more">See more</p>
+                            )}
 
-                    </div>
-                ))}
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
