530d249425667a86b01445cc037138dc42bca673
Asalagi <mypaintedfrog@yahoo.com>
2024-02-23 09:22:23 -0600

    add json file with bloglog

--- app/blog-log/page.tsx
+++ app/blog-log/page.tsx
@@ -3,22 +3,14 @@ import { useState } from 'react';
 import './blog-styling.css';
 import Image from 'next/image';
 import topImage from './top.jpg';
+import blogPost from '../../public/post.json';
 
 export default function BlogLog () {
     const [fullPost, setFullPost] = useState(false);
 
     const showContent = () => {
         setFullPost(true)
-    }
-
-    const blogData = [
-        {
-            id: 5,
-            title: "A New Day",
-            date: "21st February 2024",
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        },
-    ];
+    };
 
     const contentWords = (content: string, maxLength: number) => {
         if (content.length <= maxLength) return content;
@@ -32,8 +24,8 @@ export default function BlogLog () {
             <div className="main-container">
                 <Image src={topImage} className="flexImage" alt="top image"/>
                 <h1 className="title">Welcome to the Blog Log</h1>
-                {blogData.map((post, id) => (
-                    <div className="post-container" key={id}>
+                {blogPost.map((post) => (
+                    <div className="post-container" key={post.id}>
                         <h2 className="post-title">{post.title}</h2>
                         <p className="post-date">{post.date}</p><br/>
                         <p className="post-content">{fullPost ? post.content : contentWords(post.content, 130)}</p>
